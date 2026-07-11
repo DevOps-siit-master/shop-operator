11ebcf4abfa55dbc3ff96d7be854a87b8b829c70
Aleksa Vukomanovic <vukomanovic.aleksa@gmail.com>
2026-07-11 17:44:56 +0200

    chore: init discord crd

--- cmd/main.go
+++ cmd/main.go
@@ -185,6 +185,13 @@ func main() {
 		setupLog.Error(err, "Failed to create controller", "controller", "wallet")
 		os.Exit(1)
 	}
+	if err := (&controller.DiscordChannelReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "Failed to create controller", "controller", "discordchannel")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
